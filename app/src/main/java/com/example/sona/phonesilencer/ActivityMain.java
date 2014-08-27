8509f13cd1d1098db488b99a10b388af796dfc55
krngrvr09 <krngrvr09@gmail.com>
2014-08-28 01:06:13 +0530

    Loud-ing, silencing, vibrating phone in C, B, and A wing of 4th floor respectively.

--- app/src/main/java/com/example/sona/phonesilencer/ActivityMain.java
+++ app/src/main/java/com/example/sona/phonesilencer/ActivityMain.java
@@ -1,12 +1,15 @@
 package com.example.sona.phonesilencer;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,7 +18,8 @@ import android.widget.Toast;
 public class ActivityMain extends Activity {
 
     public static WifiManager mWifiManager;
-    public static String x;
+    public static int x;
+    public static String bssid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +27,31 @@ public class ActivityMain extends Activity {
         setContentView(R.layout.activity_main);
         //mWifiManager = (WifiManager) getSystemService(getApplicationContext().WIFI_SERVICE);
         //IntentFilter intentFilter = new IntentFilter();
-
+        final Context context = getApplicationContext();
         //registerReceiver(mWiFiBroadcastReceiver, intentFilter);
         //Log.d("wifi",x);
+        /*final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, "hahahaha ", Toast.LENGTH_SHORT).show();
+                Log.d("haha","haha");
+            }
+        }, 1000);*/
+        x=0;
+
+        new Thread(new ScanAPs()).start();
+
         WifiManager mWiFiManager = (WifiManager) getSystemService(getApplicationContext().WIFI_SERVICE);
         WifiInfo w = mWiFiManager.getConnectionInfo();
         Log.d("scanresults", String.valueOf(mWiFiManager.getScanResults()));
-        Toast.makeText(this, "APN Name = "+w.getBSSID(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "APN Name = " + w.getBSSID(), Toast.LENGTH_SHORT).show();
         Button silence = (Button) findViewById(R.id.silenceButton);
         final TextView bssid1 = (TextView) findViewById(R.id.bssid1);
         final TextView bssid2 = (TextView) findViewById(R.id.bssid2);
-
-        /*silence.setOnClickListener(new View.OnClickListener() {
+        bssid1.setText(bssid);
+        bssid2.setText(String.valueOf(x));
+        silence.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Toast.makeText(getApplicationContext(), "Silencing...", Toast.LENGTH_SHORT).show();
@@ -45,8 +62,10 @@ public class ActivityMain extends Activity {
                 bssid1.setText(s.getBSSID());
 
 
-        });*/
-        /*Button loud = (Button) findViewById(R.id.loud);
+            }
+        });
+
+        Button loud = (Button) findViewById(R.id.loud);
         loud.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -57,8 +76,8 @@ public class ActivityMain extends Activity {
                 WifiInfo w = mWiFiManager.getConnectionInfo();
                 bssid2.setText(w.getBSSID());
 
-        });*/
-
+            }
+        });
     }
     /*final BroadcastReceiver mWiFiBroadcastReceiver = new BroadcastReceiver() {
 
@@ -72,13 +91,11 @@ public class ActivityMain extends Activity {
                 sb.append("\n");
                 sb.append("SIGNAL: " + sr.level);
                 sb.append("\n");
-
             }
             x = sb.toString();
         }
 
     };*/
-
 
 
     @Override
@@ -99,4 +116,40 @@ public class ActivityMain extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    class ScanAPs implements Runnable {
+        @Override
+        public void run() {
+            for(int i=0;i<100;i++){
+                Log.d("haha","haha"+String.valueOf(x));
+                WifiManager mWiFiManager = (WifiManager) getSystemService(getApplicationContext().WIFI_SERVICE);
+                WifiInfo s = mWiFiManager.getConnectionInfo();
+                bssid = s.getBSSID();
+                Log.d("bssid",bssid+"+"+bssid.equals("c4:0a:cb:2d:ca:8a"));
+
+                if(/*bssid.equals("c4:0a:cb:2d:ca:85")||bssid.equals("b0:00:b4:20:77:23")||bssid.equals("c4:0a:cb:2d:98:3a")||*/bssid.equals("c4:0a:cb:2d:ca:8a")){
+                    Log.d("match","match");
+                    AudioManager audiomanage = (AudioManager)getSystemService(getApplicationContext().AUDIO_SERVICE);
+                    audiomanage.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                }
+                else if(bssid.equals("c4:0a:cb:2d:81:b5")){
+                    AudioManager audiomanage = (AudioManager)getSystemService(getApplicationContext().AUDIO_SERVICE);
+                    audiomanage.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+
+                }
+                else{
+                    AudioManager audiomanage = (AudioManager)getSystemService(getApplicationContext().AUDIO_SERVICE);
+                    audiomanage.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+
+                }
+                x++;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
 }
